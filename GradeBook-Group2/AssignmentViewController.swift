9f3ad062303f75e51f46d903ffb71d9e991b8422
Kristen Cassacia <kristencassacia@gmail.com>
2017-12-10 02:16:41 -0800

    Revert "12/10 2:09AM"
    This reverts commit 643ee2c6171392680444e58c89a75f6bef6a33c5.

--- GradeBook-Group2/AssignmentViewController.swift
+++ GradeBook-Group2/AssignmentViewController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit
+import FirebaseAuth
+import RNNotificationView
+import Firebase
 
 class AssignmentViewController: UIViewController {
 
     @IBAction func backdidtapped(_ sender: Any) {
         performSegue(withIdentifier: "assignmentBackToMain", sender: self)
+    }
+    
+   
+    @IBOutlet weak var pointsPossibleTextField: UITextField!
+    @IBOutlet weak var assignmentTextField: UITextField!
+    
+    @IBAction func addAssignmentdidtapped(_ sender: Any) {
+        print("add assignment did tapped")
+        let userid = Firebase.Auth.auth().currentUser?.uid
+        let ref = Database.database().reference(fromURL: "https://gradebook-group2.firebaseio.com/")
+        //let userReference = ref.child("users").child(userid!)
+        var studentID: NSArray = []
+        // let studentRef = ref.child().child(userid!)
+        let assignment = assignmentTextField.text
+        print(assignment)
+        let pointsPossible = pointsPossibleTextField.text
+        print(pointsPossible)
+        let value = ["assignment":assignment]
+        ref.child("users").child(userid!).child(pointsPossible!).setValue(value)
+        // studentRef.updateChildValues(value)
+        // let assignmentListRef = studentRef.child("AssignmentList")
+        RNNotificationView.show(withImage: UIImage(named: "sambleIcon"),
+                                title: "Congratulations!",
+                                message: "You have successfully added a new assignment.",
+                                duration: 2,
+                                iconSize: CGSize(width: 22, height: 22), // Optional setup
+            onTap: {
+                print("Did tap notification")
+        }
+        )
     }
     override func viewDidLoad() {
         super.viewDidLoad()
