f6c64e3633e2e69c85c3524e0564628368b41205
Kristen Cassacia <kristencassacia@gmail.com>
2017-12-10 02:05:15 -0800

    Joseph Changes

--- GradeBook-Group2/CategoryViewController.swift
+++ GradeBook-Group2/CategoryViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import RNNotificationView
 
 class CategoryViewController: UIViewController {
 
     @IBAction func backdidtapped(_ sender: Any) {
         performSegue(withIdentifier: "categoryBackToMain", sender: self)
     }
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    
+    
+    @IBAction func addCategorydidtapped(_ sender: UIButton) {
+        print("add category did tapped")
+        let userid = Firebase.Auth.auth().currentUser?.uid
+        let ref = Database.database().reference(fromURL: "https://gradebook-group2.firebaseio.com/")
+        //let userReference = ref.child("users").child(userid!)
+        // let studentRef = ref.child().child(userid!)
+        if let category = categoryTextField.text{
+            print(category)
+        }
+        let category = categoryTextField.text
+        print(category)
+        let weight = weightTextField.text
+        print(weight)
+        let value = ["category":category]
+        ref.child("categories").child(userid!).child(weight!).setValue(value)
+        // studentRef.updateChildValues(value)
+        // let assignmentListRef = studentRef.child("AssignmentList")
+        RNNotificationView.show(withImage: UIImage(named: "sambleIcon"),
+                                title: "Congratulations!",
+                                message: "You have successfully added a new category.",
+                                duration: 2,
+                                iconSize: CGSize(width: 22, height: 22), // Optional setup
+            onTap: {
+                print("Did tap notification")
+        }
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
